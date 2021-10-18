73138db7e24b5d4265b1afbcf1123ec49d5a713d
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-10-18 13:28:53 +0800

    menambahkan beberapa route

--- routes/admin.php
+++ routes/admin.php
@@ -21,4 +21,61 @@ Route::group(['middleware' => ['auth', 'role:admin']], function () {
 
     Route::get('/admin-kegiatan', [AdminController::class, 'kegiatan'])
         ->name('admin-kegiatan');
+
+    // fakultas
+    Route::get('/admin-fakultas', [AdminController::class, 'fakultas'])
+        ->name('admin-fakultas');
+
+    Route::get('/add-fakultas', [AdminController::class, 'addFakultas'])
+        ->name('add-fakultas');
+
+    Route::post('/add-fakultas', [AdminController::class, 'saveFakultas'])
+        ->name('add-fakultas');
+
+    Route::get('/edit-fakultas', [AdminController::class, 'editFakultas'])
+        ->name('edit-fakultas');
+
+    Route::put('/edit-fakultas', [AdminController::class, 'updateFakultas'])
+        ->name('edit-fakultas');
+
+    Route::delete('/delete-fakultas', [AdminController::class, 'destroyFakultas'])
+        ->name('delete-fakultas');
+
+    // Prodi
+    Route::get('/admin-prodi', [AdminController::class, 'prodi'])
+        ->name('admin-prodi');
+
+    Route::get('/add-prodi', [AdminController::class, 'addProdi'])
+        ->name('add-prodi');
+
+    Route::post('/add-prodi', [AdminController::class, 'saveProdi'])
+        ->name('add-prodi');
+
+    Route::get('/edit-prodi', [AdminController::class, 'editProdi'])
+        ->name('edit-prodi');
+
+    Route::put('/edit-prodi', [AdminController::class, 'updateProdi'])
+        ->name('edit-prodi');
+
+    Route::delete('/delete-prodi', [AdminController::class, 'destroyProdi'])
+        ->name('delete-prodi');
+
+    // angkatan
+    Route::get('/admin-angkatan', [AdminController::class, 'angkatan'])
+        ->name('admin-angkatan');
+
+    Route::get('/add-angkatan', [AdminController::class, 'addAngkatan'])
+        ->name('add-angkatan');
+
+    Route::post('/add-angkatan', [AdminController::class, 'saveAngkatan'])
+        ->name('add-angkatan');
+
+    Route::get('/edit-angkatan', [AdminController::class, 'editAngkatan'])
+        ->name('edit-angkatan');
+
+    Route::put('/edit-angkatan', [AdminController::class, 'updateAngkatan'])
+        ->name('edit-angkatan');
+
+    Route::delete('/delete-angkatan', [AdminController::class, 'destroyAngkatan'])
+        ->name('delete-angkatan');
 });
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::group(['middleware' => ['auth']], function () {
